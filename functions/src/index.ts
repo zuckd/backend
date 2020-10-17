c72ebae735431c1e51c07f6ed3bbe78d7ea89c8b
Ishraque99 <izahin@live.com>
2020-10-18 00:48:15 +1000

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,53 +1,70 @@
 import * as functions from 'firebase-functions';
 import { FaceClient } from "@azure/cognitiveservices-face";
 import { CognitiveServicesCredentials } from "@azure/ms-rest-azure-js";
+import admin = require("firebase-admin");
+import { HttpsError } from 'firebase-functions/lib/providers/https';
 // import Axios from "axios";
 // import { FaceListListOptionalParams, PersonGroupListOptionalParams } from '@azure/cognitiveservices-face/esm/models';
-
-const DEFAULT_PERSON_GROUP = "6939bd76-c104-4831-9a3f-1ec969e32a1b"; // Presidents
+admin.initializeApp();
+// const DEFAULT_PERSON_GROUP = "6939bd76-c104-4831-9a3f-1ec969e32a1b"; // Presidents
+const GROUP_ID = "hackathondefault1"
 // const DEFAULT_PERSON_ID = "177f9642-d489-462c-85fa-f8f25313f523"; // Obama
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
 export const addFace = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
-        return "Bad Request";
+        throw new HttpsError("unauthenticated", "Request had invalid credentials.");
     }
     const image = data.image;
-    const pid = data.personId;
-    const group = data.groupId;
+    let pid = data.personId;
     const client = getClient();
-    await client.personGroupPerson.addFaceFromStream(group, pid, image);
-    await client.personGroup.train(group);
-    return "Done";
+    if (pid) {
+        await client.personGroupPerson.addFaceFromStream(GROUP_ID, pid, image);
+    } else {
+        const newPerson = await client.personGroupPerson.create(GROUP_ID);
+        await client.personGroupPerson.addFaceFromStream(GROUP_ID, newPerson.personId, image);
+        pid =  newPerson.personId;
+    }
+    await client.personGroup.train(GROUP_ID);
+    return pid;
 });
 
-export const getFaceId = functions.https.onCall(async (data, context) => {
+export const getPersonId = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
-        return "Bad Request";
+        throw new HttpsError("unauthenticated", "Request had invalid credentials.");
     }
     const image = data.image;
-    const group = data.groupId || DEFAULT_PERSON_GROUP;
     const client = getClient();
     const faces = await client.face.detectWithStream(image, {detectionModel: "detection_02"});
     let faceIds: string[] = []
     faces.forEach(face => faceIds.push(face.faceId as string));
-    const resp = await client.face.identify(faceIds, {personGroupId: group});
+    const resp = await client.face.identify(faceIds, {personGroupId: GROUP_ID});
     let results: string[] = []
     resp.forEach(r => results.push(r.candidates[0].personId));
     return results;
 });
 
-export const createPerson = functions.https.onCall(async (data, context) => {
-    if (!context.auth) { return "Bad Request" };
-    const image = data.image;
-    const group = data.groupId;
-    const client = getClient();
-    const newPerson = await client.personGroupPerson.create(group);
-    await client.personGroupPerson.addFaceFromStream(group, newPerson.personId, image);
-    return newPerson.personId;
-});
+// export const onSignOn = functions.auth.user().onCreate((user, context) => {
+//     const db = admin.firestore();
+//     db.collection("users").doc(user.uid).update({
+//         group: GROUP_ID,
+//         personId: 
+//     });
+// });
+
+// export const testGroup = functions.https.onRequest( async (req, res) => {
+//     const client = getClient();
+//     let result = "";
+//     try {
+//         const resp = await client.personGroup.create("hackathondefault1", {
+//             recognitionModel: "recognition_03",
+//             name: "hackathondefault1"
+//         });
+//         result = resp.toString();
+//     } catch (e) {
+//         console.log(e);
+//         result = e.toString();
+//     }
+//     res.send(result);
+// });
 
 const getClient = () => {
     const faceKey = functions.config().face.key as string;
